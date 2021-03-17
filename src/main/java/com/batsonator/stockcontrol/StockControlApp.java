de5cdd276a334866243f9c78c4ce6da6df7b2fbc
Dan Batson <batsond@outlook.com>
2021-03-17 16:43:54 +0000

    first commit

---
+++ src/main/java/com/batsonator/stockcontrol/StockControlApp.java
@@ -0,0 +1,13 @@
+package com.batsonator.stockcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockControlApp {
+
+	public static void main( String[] args ) {
+
+		SpringApplication.run( StockControlApp.class, args );
+	}
+}
